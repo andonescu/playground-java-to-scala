8772ab3d484880dbbdcd65c9118e21885ad20ff9
Ionut Andonescu <andonescu@gmail.com>
2016-03-13 19:12:11 +0200

    improve the generic in Pair class + add both methods in StudentProcessor class

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,16 +1,22 @@
 package main.java;
 
-import java.util.Set;
-
 /**
  * Created by Ionut on 10.03.2016.
  */
 public class Pair<T, V> {
-    public final Set<T> _1;
-    public final Set<T> _2;
+    public final T _1;
+    public final V _2;
 
-    public Pair(Set<T> _1, Set<T> _2) {
+    public Pair(T _1, V _2) {
         this._1 = _1;
         this._2 = _2;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "_1=" + _1 +
+                ", _2=" + _2 +
+                '}';
     }
 }
--- src/main/java/StudentProcessor.java
+++ src/main/java/StudentProcessor.java
@@ -15,20 +15,40 @@ public class StudentProcessor {
     /**
      * Separate students in two distinct buckets <romania & other>
      */
-    public Pair<Student, Student> getStudentsByOriginCountry(Set<Student> students) {
+    public Pair<Student, Student> getStudentsByOriginCountry_01(Set<Student> students) {
+
+        Set<Student> romaniaStudents = new HashSet<Student>();
+        Set<Student> otherStudents = new HashSet<Student>();
+
+        for (Student student : students) {
+            if (student.getAddress().getCountry().equals("Romania")) {
+                romaniaStudents.add(student);
+            } else {
+                otherStudents.add(student);
+            }
+        }
+
+        return new Pair(romaniaStudents, otherStudents);
+    }
+
+
+    /**
+     * Separate students in two distinct buckets <romania & other>
+     */
+    public Pair<Set<Student>, Set<Student>> getStudentsByOriginCountry_02(Set<Student> students) {
 
         Predicate<Student> isRomanian = (student) -> student.getAddress().getCountry().equals("Romania");
 
-        BiConsumer<Pair<Student, Student>, Student> consumer = (pair, st) -> {
+        BiConsumer<Pair<Set<Student>, Set<Student>>, Student> consumer = (pair, st) -> {
             if (isRomanian.test(st)) pair._1.add(st);
             else pair._2.add(st);
         };
 
-        Supplier<Pair<Student, Student>> supplier = () -> new Pair<>(new HashSet<>(), new HashSet<>());
+        Supplier<Pair<Set<Student>, Set<Student>>> supplier = () -> new Pair<>(new HashSet<>(), new HashSet<>());
 
         return students.stream().collect(
                 Collector.of(
-                        () -> new Pair<>(new HashSet<>(), new HashSet<>()),
+                        supplier,
                         consumer,
                         (left, right) -> {
                             left._1.addAll(right._1);
